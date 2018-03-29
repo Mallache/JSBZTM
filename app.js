2002a44330e455a9b5f11b90a1c77d69bcb45781
lutong <tonglu0710@163.com>
2018-03-29 16:07:31 +0800

    增加我的

--- app.js
+++ app.js
@@ -9,30 +9,51 @@ App({
   onLaunch: function () {
     this.getUserInfo();
   },
-  getUserInfo: function () {
+  /*getUserInfo: function () {
+    var that = this; 
     wx.login({
       success: function (res) {
         if (res.code) {
           wx.getUserInfo({
             success: function (res) {
+              that.globalData.userInfo = res.userInfo; 
               wx.setStorage({
                 key: 'userInfo',
                 data: res.userInfo,
               })
               console.log(res.userInfo);
+             
             }
           });
         }else{
 
         }
-      }
+      }*/
+
+  getUserInfo: function (cb) {
+    var that = this;
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口  
+      wx.login({
+        success: function () {
+          wx.getUserInfo({
+            success: function (res) {
+              that.globalData.userInfo = res.userInfo;
+              typeof cb == "function" && cb(that.globalData.userInfo)
+            }
+          })
+        }
+      
 
     })
-
+    }
   },
+
   globalData: {
     openId: null,
-
+    userInfo: null,
   },
 
 })
